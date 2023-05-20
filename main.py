f73e4456e8e26b738393c453b96f8d2123e48696
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2023-05-20 16:58:42 -0300

    codigo do projeto

---
+++ main.py
@@ -0,0 +1,22 @@
+import uvicorn
+from fastapi import fastAPI
+
+from src.responses import TrendItem
+from src.services import  get_trends_from_mongo, save_trends
+
+
+app = fastAPI()
+
+
+@app.get('/trends', response_model=List[TrendItem])
+def get_trends_route():
+    return get_trends_from_mongo()
+
+
+if __name__ == '__main__':
+    trends = get_trends_from_mongo()
+
+    if not trends:
+        save_trends()
+    
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ src/conection.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://dio:dio@localhost:27017/")
+
+db = client.dio_live
+
+trends_collection = db.trends
+
+# print(list(tweets))
